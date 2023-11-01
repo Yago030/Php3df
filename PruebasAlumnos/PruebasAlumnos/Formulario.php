af94cc3814a7af57c45bb307e830b68bc55a0c2c
Bergerat Santiago <70027319+Yago030@users.noreply.github.com>
2023-11-01 11:24:29 -0300

    subiendo

---
+++ PruebasAlumnos/PruebasAlumnos/Formulario.php
@@ -0,0 +1,22 @@
+<html>
+  <head>
+  </head>
+  <body>
+
+      <form action="php/procesa.php" method="post">
+
+      <label> Ingrese su Nombre:</label>
+      <input type="text" name="nombre" required> <br>
+
+      <label> Ingrese su Apellido:</label>
+      <input type="text" name="apellido" required> <br>
+
+      <label> Ingrese su Empresa:</label>
+      <input type="text" name="empresa" required> <br>
+
+      <input type="submit" value="Enviar">
+
+      </form>
+
+  </body>
+</html>
